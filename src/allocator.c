9cd8d45218647e8134c959b62938546159261822
AyushChakraborty <chakrabortyayush83@gmail.com>
2024-10-19 01:00:23 +0530

    fix: Change mem_calloc() function

--- src/allocator.c
+++ src/allocator.c
@@ -82,16 +82,16 @@ u_int16_t add_to_free_list(alloc_t *allocator, block_t *block) {
 }
 
 void *mem_alloc(alloc_t *allocator, size_t num_units) {
-  if (allocator->allocated_size > ALLOC_LIMIT) {
+  if (allocator->allocated_size > ALLOC_LIMIT) {  //if allocator size is more than limit
     gc_collect(allocator);
   }
 
   size_t total_size = (num_units + sizeof(block_t) - 1) / sizeof(block_t) + 1;
-  total_size *= sizeof(block_t);
+  total_size *= sizeof(block_t);   //gives the total byte size needed for allocation
+ 
+  block_t *free_list = allocator->free_list;   //init pointer to the free list
 
-  block_t *free_list = allocator->free_list;
-
-  do {
+  do {  
     if (free_list->size >= total_size) {
       if (free_list->size == total_size) {
         free_list->prev->next = free_list->next;
@@ -149,6 +149,77 @@ void *mem_alloc(alloc_t *allocator, size_t num_units) {
 }
 
 
+void *mem_calloc(alloc_t *allocator, size_t num_units) {
+  if (allocator->allocated_size > ALLOC_LIMIT) { 
+    gc_collect(allocator);
+  }
+
+  size_t total_size = (num_units + sizeof(block_t) - 1) / sizeof(block_t) + 1;
+  total_size *= sizeof(block_t);   
+ 
+  block_t *free_list = allocator->free_list;  
+
+  do {  
+    if (free_list->size >= total_size) {
+      if (free_list->size == total_size) {
+        free_list->prev->next = free_list->next;
+        if (free_list->next != NULL) {
+          free_list->next->prev = free_list->prev;
+        }
+
+        free_list->next = allocator->used_list->next;
+        if (allocator->used_list->next != NULL) {
+          allocator->used_list->next->prev = free_list;
+        }
+        free_list->prev = allocator->used_list;
+        allocator->used_list->next = free_list;
+
+        allocator->allocated_size += free_list->size;
+		memset((void*)(free_list+1), 0, total_size);
+        return (void *)(free_list + 1);
+      }
+      if (free_list->size - total_size >= sizeof(block_t)) {
+        block_t *new_block = (block_t *)((char *)free_list + total_size);
+        new_block->size = free_list->size - total_size;
+        new_block->next = free_list->next;
+        new_block->prev = free_list->prev;
+        free_list->prev->next = new_block;
+        if (free_list->next != NULL) {
+          free_list->next->prev = new_block;
+        }
+
+        free_list->size = total_size;
+        free_list->next = allocator->used_list->next;
+        if (allocator->used_list->next != NULL) {
+          allocator->used_list->next->prev = free_list;
+        }
+        free_list->prev = allocator->used_list;
+        allocator->used_list->next = free_list;
+
+        allocator->allocated_size += free_list->size;
+        return (void *)(free_list + 1);
+      }
+    }
+    free_list = free_list->next;
+  } while (free_list != allocator->free_list);
+
+  size_t total_pages = (total_size + PAGE_SIZE - 1) / PAGE_SIZE;
+  block_t *new_block = mempage_fetch(total_pages);
+  if (new_block == NULL) {
+    return NULL;
+  }
+
+  new_block->next = NULL;
+  new_block->prev = NULL;
+  new_block->size = total_pages * PAGE_SIZE;
+
+  add_to_free_list(allocator, new_block);
+  memset((void *)(new_block + 1), 0, new_block->size - sizeof(block_t)); // Ensure zeroing for the new block
+
+  return mem_alloc(allocator, num_units);
+}
+
+
 u_int16_t mem_dealloc(alloc_t *allocator, void *mem) {
   block_t *block = (block_t *)mem - 1;
 
@@ -160,7 +231,7 @@ u_int16_t mem_dealloc(alloc_t *allocator, void *mem) {
     block->next->prev = block->prev;
   }
 
-  // add the block back to the free list
+ 
   if (add_to_free_list(allocator, block) == ALLOC_FAILURE) {
     return ALLOC_FAILURE;
   }
